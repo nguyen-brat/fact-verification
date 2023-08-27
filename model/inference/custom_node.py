6e4c78e771a9e2e48e4eef686f19a36d494e625f
nguyen-brat <julius.2003.no.2@gmail.com>
2023-08-28 00:02:53 +0700

    init

---
+++ model/inference/custom_node.py
@@ -0,0 +1,19 @@
+from haystack import BaseComponent
+from typing import Optional, List
+
+
+class fact_verification(BaseComponent):
+    outgoing_edges = 1
+
+    def run(self, query: str, my_optional_param: Optional[int]):
+        # process the inputs
+        output = {"my_output": ...}
+        return output, "output_1"
+
+    def run_batch(self, queries: List[str], my_optional_param: Optional[int]):
+        # process the inputs
+        output = {"my_output": ...}
+        return output, "output_1"
+
+if __name__ == '__main__':
+    pass
