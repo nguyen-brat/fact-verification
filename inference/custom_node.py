1c70d7c9bb39257dd2245312e712042dc484038b
nguyen-brat <julius.2003.no.2@gmail.com>
2023-08-28 17:46:54 +0700

    x

--- inference/custom_node.py
+++ inference/custom_node.py
@@ -1,12 +1,18 @@
 from haystack import BaseComponent
 from typing import Optional, List
+from model.claim_verification.gear.model import fact_verification
+from sentence_transformers.cross_encoder import CrossEncoder
 
 class reranking(BaseComponent):
     outgoing_edges = 1
-
-    def run(self, query: str, my_optional_param: Optional[int]):
+    def __init__(self, path):
+        self.model = CrossEncoder(path)
+    def run(self, query: str, contexts:List[str]):
         # process the inputs
-        output = {"my_output": ...}
+        output = self.model(
+            query,
+            contexts
+        )
         return output, "output_1"
 
     def run_batch(self, queries: List[str], my_optional_param: Optional[int]):
@@ -16,7 +22,8 @@ class reranking(BaseComponent):
 
 class fact_verification(BaseComponent):
     outgoing_edges = 1
-
+    def __init__(self, path):
+        self.model = fact_verification.from_pretrained(path)
     def run(self, query: str, my_optional_param: Optional[int]):
         # process the inputs
         output = {"my_output": ...}
