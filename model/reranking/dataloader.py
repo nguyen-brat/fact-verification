89504dc8291c7886bff257845e997e0da7a788b2
nguyen-brat <julius.2003.no.2@gmail.com>
2023-08-30 22:23:12 +0700

    x

---
+++ model/reranking/dataloader.py
@@ -0,0 +1,41 @@
+from torch.utils.data import Dataset
+from sentence_transformers.readers import InputExample
+import collections
+import glob
+import logging
+import os
+import random
+from typing import Dict, List, Tuple
+
+CrossEncoderPassage = collections.namedtuple("CrossEncoderPassage", ["text", "title"])
+
+class CrossEncoderSample(object):
+    query: str
+    positive_passages: List[CrossEncoderPassage]
+    negative_passages: List[CrossEncoderPassage]
+    hard_negative_passages: List[CrossEncoderPassage]
+
+CrossEncoderBatch = List[InputExample]
+
+class dataloader(Dataset):
+    def __init__(
+            self,
+            data_path,
+    ):
+        self.data_path = data_path
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+    
+    def create_biencoder_input(
+            samples: List[CrossEncoderSample],
+            insert_title: bool,
+            num_hard_negatives: int = 0,
+            num_other_negatives: int = 0,
+            shuffle: bool = True,
+            shuffle_positives: bool = False,
+    )->CrossEncoderBatch:
+        pass
--- model/reranking/trainer.py
+++ model/reranking/trainer.py
@@ -1,14 +1,16 @@
-from sentence_transformers.cross_encoder import CrossEncoder
+from .model import CrossEncoder
+from .dataloader import dataloader
+from torch.utils.data import DataLoader
 import math
 
-class trainer:
+class Trainer:
     def __init__(
             self,
             dataloader,
-            MODEL:str='xlm-roberta-base',
+            model:str='amberoad/bert-multilingual-passage-reranking-msmarco',
     ):
         self.dataloader = dataloader
-        self.model = CrossEncoder(MODEL, num_labels=2)
+        self.model = CrossEncoder(model, num_labels=2)
         self.warnmup_step = math.ceil(len(dataloader) * 10 * 0.1)
     
     def __call__(self, epochs:int=10):
@@ -18,7 +20,10 @@ class trainer:
             evaluation_steps=10000,
             warmup_steps=self.warnmup_step,
         )
-        self.model.save_pretrained("reranking_model") 
+        self.model.save_pretrained("model/reranking/saved_model") 
 
 if __name__ == "__main__":
-    pass
+    train_data = dataloader('data_path')
+    train_dataloader = DataLoader(train_data)
+    trainer = Trainer(train_data)
+    trainer(epochs=10)
