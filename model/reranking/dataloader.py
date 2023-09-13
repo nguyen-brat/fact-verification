ba8ec540dd7715f41b28f5011c8093ba2b021b13
nguyen-brat <julius.2003.no.2@gmail.com>
2023-09-14 01:38:36 +0700

    update daloader of rerank

--- model/reranking/dataloader.py
+++ model/reranking/dataloader.py
@@ -6,47 +6,83 @@ import logging
 import os
 import random
 from typing import Dict, List, Tuple
+import multiprocessing
 from glob import glob
+from underthesea import sent_tokenize
 
-class CrossEncoderSample(object):
-    query: str
-    positive_passages: List[str]
-    negative_passages: List[str]
-    hard_negative_passages: List[str]
+class CrossEncoderSamples(object):
+    query: List[str] = []
+    positive_passages: List[str] = []
+    contexts: List[str] = []
 
 CrossEncoderBatch = List[InputExample] # [[claim, answer], [claim, answer]]
+
+class DataloaderConfig(object):
+    num_hard_negatives:int=1,
+    num_other_negatives:int=7,
+    shuffle:bool=True,
+    shuffle_positives:bool=True,
+    batch_size:int=16,
 
 class dataloader(Dataset):
     def __init__(
             self,
             data_path,
-            num_hard_negatives=1,
-            num_other_negatives=7,
-            shuffle=True,
-            shuffle_positives=True,
+            config:DataloaderConfig,
     ):
-        self.data_path = glob('dump_data/train' + '/*/*.json')
-        if shuffle:
+        self.config = config
+        self.data_paths = glob('dump_data/train' + '/*/*.json')
+        if config.shuffle:
             random.shuffle(self.data_path)
 
     def __len__(self):
-        pass
+        return len(self.data_path)//self.batch_size
 
     def __getitem__(self, idx):
-        pass
+        return self.create_biencoder_input(idx=idx)
     
-    def create_biencoder_input(
-            samples: List[CrossEncoderSample],
-            num_hard_negatives: int = 0,
-            num_other_negatives: int = 0,
-            shuffle: bool = True,
-            shuffle_positives: bool = False,
-    )->CrossEncoderBatch: 
-        pass
+    def create_biencoder_input(self, idx)->CrossEncoderBatch: 
+        raw_batch = self.create_crossencoder_samples(idx)
     
+    def create_crossencoder_samples(self, idx)->CrossEncoderSamples:
+        def read_contexts(diction):
+            return sent_tokenize(diction['context'])
+        def read_claims(diction):
+            return diction['claim']
+        def read_evidients(diction):
+            return diction['evident']
+
+        id = idx*self.config.batch_size
+        samples = CrossEncoderSamples
+        raw_data = self.read_files(self.data_paths[id:(id+self.config.batch_size)])
+        with multiprocessing.Pool() as pool:
+            contexts = pool.map(read_contexts, raw_data)
+            claims = pool.map(read_claims, raw_data)
+            evidents = pool.map(read_evidients, raw_data)
+        contexts_set = set()
+        for context in contexts:
+            contexts_set.update(context)
+        samples.contexts = list(contexts_set)
+        samples.query = claims
+        samples.positive_passages = evidents
+        
+        return samples
+        
+
     @staticmethod
-    def create_crossencoder_sample(path, batch_size):
-        '''
-        create crossencoder sample from file path
-        '''
-        pass
+    def read_files(paths):
+        def read_file(file):
+            with open(file, 'r') as f:
+                data = f.read()
+            return data
+    
+        with multiprocessing.Pool() as pool:
+            results = pool.map(read_file, paths)
+        return results
+
+    @staticmethod
+    def read_file(file):
+        with open(file, 'r') as f:
+            data = f.read()
+        return data
+        
