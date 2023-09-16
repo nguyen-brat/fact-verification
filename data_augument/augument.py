2256dfda0c7db34af2749cdf9769e11a194d83a7
DuyEz13 <100853038+DuyEz13@users.noreply.github.com>
2023-09-16 17:07:59 +0700

    Update augument.py

--- data_augument/augument.py
+++ data_augument/augument.py
@@ -2,22 +2,85 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset
 import json
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import multiprocessing
+import glob
+from glob import glob
+import random
+from transformers import MT5Tokenizer, MT5ForConditionalGeneration
+from underthesea import sent_tokenize
+
+CKPT = 'chieunq/vietnamese-sentence-paraphase'
+tokenizer_pr = MT5Tokenizer.from_pretrained(CKPT)
+model_pr = MT5ForConditionalGeneration.from_pretrained(CKPT)
 
 class DataAugmentation(Dataset):
   def __init__(self,
-               data_path):
-    self.data_path = data_path
-    with open(data_path, 'r') as f:
-            self.data = json.load(f)
+               data_path,
+               num_data,
+               paraphrase: bool = False,
+               back_translate: bool = False,
+               remove_stopwords: bool = False):
+    self.data_paths = glob(data_path + '/*/*.json')
+    random.shuffle(self.data_paths)
+    self.num_data = num_data
+    self.raw_data = self.read_files(self.data_paths[:num_data])
+    self.raw_data = pd.DataFrame(self.raw_data)
+    self.raw_context = self.raw_data['context'].map(self.split_doc)
+    self.eidx = self.index_of_evidient()
+    if(paraphrase):
+      self.paraphrase = self.llm_paraphrase()
+    if(back_translate):
+      self.back_translate_data = self.back_translation()
 
-    @staticmethod
-    def llm_paraphrase(self):
-        pass
+  @staticmethod
+  def split_doc(graphs):
+    output = sent_tokenize(graphs)
+    in_element = list(map(lambda x:x[:-1].strip(), output[:-1]))
+    last_element = output[-1] if (output[-1][-1] != '.') else output[-1][-1].strip()
+    return in_element + [last_element]
+  
+  def index_of_evidient(self):
+    list_of_idx = []
+    for i in range(self.num_data):
+      if(self.raw_data['verdict'][i] != "NEI"):
+        list_of_idx.append(self.raw_context[i].index(self.raw_data['evidient'][i]))
+      else:
+        list_of_idx.append(-1)
+    return list_of_idx
+  
 
-    @staticmethod
-    def back_translation(self):
-        pass
+  def pr(self, text):
+    inputs = tokenizer_pr(text, padding='longest', max_length=64, return_tensors='pt')
+    input_ids = inputs.input_ids
+    attention_mask = inputs.attention_mask
+    output = model_pr.generate(input_ids, attention_mask=attention_mask, max_length=64)
+    return tokenizer_pr.decode(output[0], skip_special_tokens=True)
 
-    @staticmethod
-    def remove_stopwords(self):
-        pass
+  def llm_paraphrase(self):
+    new_data = self.raw_data.copy()
+    new_data['context'] = new_data['context'].apply(self.pr)
+    new_data['claim'] = new_data['claim'].apply(self.pr)
+    raw_context = new_data['context'].map(self.split_doc)
+    for i in range(self.num_data):
+      if(self.raw_data['verdict'][i] != "NEI"):
+        new_data['evidient'][i] = raw_context[i][self.eidx[i]]
+
+    return new_data
+
+  def back_translation(self):
+    new_data = []
+    return new_data
+
+  def remove_stopwords(self):
+    pass
+
+  def read_files(self, paths):
+    results = list(map(self.read_file, paths))
+    return results
+
+  @staticmethod
+  def read_file(file):
+    with open(file, 'r', encoding='utf8') as f:
+      data = json.load(f)
+    return data
