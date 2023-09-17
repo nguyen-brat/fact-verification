5f6f17891a0dbadcc537ed945b4f4402cde4b43f
nguyen-brat <julius.2003.no.2@gmail.com>
2023-09-17 14:22:02 +0700

    update

--- model/reranking/trainer.py
+++ model/reranking/trainer.py
@@ -90,6 +90,10 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def rerank_run():
+    args = parse_args()
+    main(args=args)
+
 if __name__ == "__main__":
     args = parse_args()
     main(args=args)
---
+++ train.py
@@ -0,0 +1,4 @@
+from model.reranking.trainer import rerank_run
+
+if __name__ == "__main__":
+    rerank_run()
